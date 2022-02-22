0ee3b9a447e09efe801caf79c54c1fa86b5c6a30
wangyixuan <wangyixuan@jdjt.net>
2022-02-22 11:51:20 +0800

    feat: update pom

--- src/main/java/com/xuan/demo/TestApplication.java
+++ src/main/java/com/xuan/demo/TestApplication.java
@@ -4,20 +4,10 @@ package com.xuan.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 @SpringBootApplication
 @Configuration
 class TestApplication {
-
-    @RequestMapping("hello")
-    @ResponseBody
-    public String hello() {
-        return "hello world！";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
